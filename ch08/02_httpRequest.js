7ad093b82819efbb5ae46f34a26d32ac2db834c7
Cheolkie Kim <ckiekim@naver.com>
2019-10-08 09:13:37 +0900

    Ch07-10 HTTP

---
+++ ch08/02_httpRequest.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+
+	console.log('Method : ', req.method);
+	console.log('url : ', req.url);
+	console.log('============= Headers ================')
+	console.log('headers : ', req.headers['user-agent']);
+			
+	res.write('Hello World');
+	res.end();
+}).listen(3000);
