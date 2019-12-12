04ef29750db66b8a9f927b0f4ca6b9b1f41d5a9b
Cheolkie Kim <ckiekim@naver.com>
2019-12-12 22:03:36 +0900

    SQLite3

--- SQLite/01_conn.js
+++ SQLite/01_conn.js
@@ -4,6 +4,6 @@ var db = new sqlite3.Database(file);
 db.all("SELECT * FROM Movie_reviews limit 10", function(err, rows) {
         rows.forEach(function (row) {
             console.log(row.word, row.pos, row.count);
-        })
+        });
 	});	
 db.close();
