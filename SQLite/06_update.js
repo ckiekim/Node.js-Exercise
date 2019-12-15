f9a4fa0386e63434ef9b4649427fcc336c3da941
Cheolkie Kim <ckiekim@naver.com>
2019-12-15 14:24:19 +0900

    SQLite3

--- SQLite/06_update.js
+++ SQLite/06_update.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var file = "db/my.db";
 var db = new sqlite3.Database(file);
 
-var sql = 'UPDATE bbs SET content=? WHERE id=?';
+var sql = "UPDATE bbs SET timestamp=datetime('now'), content=? WHERE id=?";
 var id = 101;
 var content = 'SQLite3 with Node.js'
 var sql2 = "SELECT id, title, writer, strftime('%m%d%H%M', timestamp, 'localtime') ts, content FROM bbs";
