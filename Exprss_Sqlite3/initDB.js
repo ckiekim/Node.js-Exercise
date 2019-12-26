1bbb3540804f760f5c1ca2637d49bb3fe60fc646
Cheolkie Kim <ckiekim@naver.com>
2019-12-26 21:39:33 +0900

    Modify Init DB program

--- Exprss_Sqlite3/initDB.js
+++ Exprss_Sqlite3/initDB.js
@@ -24,13 +24,23 @@ var createSql = `
 var insertSql = "INSERT INTO bbs(id, title, writer, content) VALUES(100, 't1', 'w1', 'content 1')";
 var selectSql = "SELECT id, title, writer, strftime('%Y-%m-%d %H:%M', timestamp, 'localtime') ts, content FROM bbs";
 var records = [
-    {title: '동백꽃 필 무렵', writer: 'dblee', content:'편견에 갇힌 맹수 동백을 깨우는, 촌므파탈 황용식이의 폭격형 로맨스 "사랑하면 다 돼!"'},     // 임상춘
+    {title: '동백꽃 필 무렵', writer: 'dboh', content:'편견에 갇힌 맹수 동백을 깨우는, 촌므파탈 황용식이의 폭격형 로맨스 "사랑하면 다 돼!"'},     // 임상춘
     {title: '미스터 션샤인', writer: 'eskim', content:'신미양요(1871년) 때 군함에 승선해 미국에 떨어진 한 소년이 미국 군인 신분으로 자신을 버린 조국인 조선으로 돌아와 주둔하며 벌어지는 일을 그린'}
 ];
+
+var userSql = `
+    CREATE TABLE IF NOT EXISTS user (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        password TEXT NOT NULL,
+        tel TEXT,
+        email TEXT)
+`;
 
 db.serialize(function() {
     db.run(createSql);
     db.run(insertSql);
+    db.run(userSql);
 
     var sql = 'INSERT INTO bbs(title, writer, content) VALUES(?, ?, ?)';
     var stmt = db.prepare(sql);
